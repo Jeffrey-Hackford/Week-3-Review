b03741193db2eb58cb26318507741a17440e5bbc
Jeffrey Hackford <jeffrey.hackford@gmail.com>
2016-04-01 15:11:54 -0700

    add row and columns for output numbers and text to further organize results

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,22 @@
  function ping(input) {
   for (var index = 1, userNumber = input; index <= userNumber; index += 1) {
     if (index % 15 === 0) {
-      $("#output").append("<p>" + "Ping-Pong" + "</p>");
+      $("#pingpong").append("<p>" + "Ping-Pong" + "</p>");
     } else if
       (index % 3 === 0) {
-      $("#output").append("<p>" + "Ping" + "</p>");
+      $("#ping").append("<p>" + "Ping" + "</p>");
     } else if
       (index % 5 === 0) {
-      $("#output").append("<p>" + "Pong" + "</p>");
+      $("#pong").append("<p>" + "Pong" + "</p>");
     } else {
-      $("#output").append("<p>" + index + "</p>");
+      $("#none").append("<p>" + index + "</p>");
     }
   }
 }
   $(document).ready(function() {
     $("#button").click(function(event) {
-      event.preventDefault();
       var newNumber = $("#userInput").val();
       ping(newNumber);
-
+      event.preventDefault();
     });
   });
