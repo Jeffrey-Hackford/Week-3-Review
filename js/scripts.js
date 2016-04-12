ca72575f67c9e6bd6a6f1829c61b597c56a5f739
Jeffrey Hackford <jeffrey.hackford@gmail.com>
2016-04-11 20:45:01 -0700

    attempt to separate business logic from user logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,59 @@
-// Start User Logic
- function ping(input) {
-  for (var index = 1, userNumber = input; index <= userNumber; index += 1) {
-    if (index % 15 === 0) {
-      $("#pingpong").append("<p>" + "Ping-Pong" + "</p>");
-    } else if
-      (index % 3 === 0) {
-      $("#ping").append("<p>" + "Ping" + "</p>");
-    } else if
-      (index % 5 === 0) {
-      $("#pong").append("<p>" + "Pong" + "</p>");
-    } else {
-      $("#none").append("<p>" + index + "</p>");
-    }
-  }
-}
-// End User Logic
+var ping = [];
+var pong = [];
+var pingPong = [];
+var otherNumbers = [];
+var output = [];
 
-// Start Business Logic
+
   $(document).ready(function() {
     $("#button").click(function(event) {
       var newNumber = $("#userInput").val();
+
+
       ping(newNumber);
 
-      event.preventDefault();
+      function ping() {
+       for (var index = 1; index <= newNumber; index += 1) {
+         if (index % 15 === 0) {
+           output.push("PingPong");
+         } else if
+           (index % 3 === 0) {
+           output.push("Ping");
+         } else if
+           (index % 5 === 0) {
+           output.push("Pong");
+         } else {
+           output.push(index);
+         }
+       }
+      console.log(output);
+     }
+
+      output.forEach(function() {
+        if (output === "Ping") {
+          $("#ping").append("<p>" + "Ping" + "</p>");
+        } else if
+          (output === "Pong") {
+          $("#pong").append("<p>" + "Pong" + "</p>");
+        } else if
+          (ouput === "PingPong") {
+          $("#pingPong").append("<p>" + "PingPong" + "</p>");
+        } else {
+          $("#none").append("<p>" + index + "</p>");
+        }
+console.log(output);
+event.preventDefault();
+      });
     });
-  });
-  
-// Dummy click function to activate default behavior
+});
+
+
   $(document).ready(function() {
     $("reset").click(function() {
     });
   });
+//
+//
+// $("#ping").append("<p>" + "Ping" + "</p>");
+// $("#pong").append("<p>" + "Pong" + "</p>");
+// $("#none").append("<p>" + index + "</p>");
