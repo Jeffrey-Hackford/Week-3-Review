859766f5c7d44a3829b5ef7aadc51f91ecdcc4b9
Jeffrey Hackford <jeffrey.hackford@gmail.com>
2016-04-13 20:04:41 -0700

    separate business logic from user logic by adding a global variable to push to and then loop through.

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,29 @@ var pingPong = [];
 var otherNumbers = [];
 var output = [];
 
+function append() {
+ for (var index = 1; index <= output.length; index += 1) {
+   if (index % 15 === 0) {
+     $("#pingpong").append("<p>" + "PingPong" + "</p>");
+   } else if
+     (index % 3 === 0) {
+     $("#ping").append("<p>" + "Ping" + "</p>");
+   } else if
+     (index % 5 === 0) {
+     $("#pong").append("<p>" + "Pong" + "</p>");
+   } else {
+     $("#none").append("<p>" + index + "</p>");
+   }
+ }
+}
 
   $(document).ready(function() {
     $("#button").click(function(event) {
+      event.preventDefault();
+
       var newNumber = $("#userInput").val();
 
-
       ping(newNumber);
-
       function ping() {
        for (var index = 1; index <= newNumber; index += 1) {
          if (index % 15 === 0) {
@@ -26,34 +41,11 @@ var output = [];
            output.push(index);
          }
        }
-      console.log(output);
      }
-
-      output.forEach(function() {
-        if (output === "Ping") {
-          $("#ping").append("<p>" + "Ping" + "</p>");
-        } else if
-          (output === "Pong") {
-          $("#pong").append("<p>" + "Pong" + "</p>");
-        } else if
-          (ouput === "PingPong") {
-          $("#pingPong").append("<p>" + "PingPong" + "</p>");
-        } else {
-          $("#none").append("<p>" + index + "</p>");
-        }
-console.log(output);
-event.preventDefault();
-      });
+     append(output);
     });
 });
-
-
   $(document).ready(function() {
     $("reset").click(function() {
     });
   });
-//
-//
-// $("#ping").append("<p>" + "Ping" + "</p>");
-// $("#pong").append("<p>" + "Pong" + "</p>");
-// $("#none").append("<p>" + index + "</p>");
